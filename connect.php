c450ea7aced8394863ecaf68b858889482ce5444
hipzulaj <hipzulachmadjabbar@gmail.com>
2016-11-30 09:49:26 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+	$nama_server = "localhost";
+	$nama_user = "root";
+	$kata_sandi = "";
+	$nama_db = "sijalas";
+	$mengoneksikan = mysqli_connect($nama_server, $nama_user, $kata_sandi, $nama_db);
+?>
